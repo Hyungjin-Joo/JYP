7bf884fd46cac9ce4d8d75a6de8baaba34f3a8a6
Hyungjin Joo <112383495+Hyungjin-Joo@users.noreply.github.com>
2023-02-01 17:11:13 +0900

    Delete code directory

--- code/stage02.bash
+++
@@ -1,9 +0,0 @@
-rm *json
-asn_make_pool st2.ecsv *rate.fits
-asn_generate st2.ecsv
-rm *image3*
-for f in *json
-do
-	echo $f
-	strun calwebb_image2 $f --steps.flat_field.skip True --steps.resample.skip  True
-done
--- code/stage03.bash
+++
@@ -1,5 +0,0 @@
-for f in *.json
-do
-    echo $f
-    strun $JYP/drizzle.image3 $f
-done
